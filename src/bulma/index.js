1c3a30c784208011ae27142d1dffab5b33762c00
reza <raftx24@gmail.com>
2020-11-05 00:27:56 +0330

    improves logic

--- src/bulma/index.js
+++ src/bulma/index.js
@@ -2,18 +2,11 @@ import Vue from 'vue';
 import App from '@enso-ui/ui/src/bulma/app';
 import './icons';
 import routeImporter from '@enso-ui/ui/src/modules/importers/routeImporter';
-import RouteMerger from '@enso-ui/ui/src/modules/importers/RouteMerger';
 import Navbar from './components/navbar/Tasks.vue';
 
-const routes = [];
+const { $store: { commit }, $router } = App.root;
 
-(new RouteMerger(routes))
-    .add(routeImporter(require.context('./routes', false, /.*\.js$/)));
+$router.addRoutes(routeImporter(require.context('./routes', false, /.*\.js$/)));
 
 Vue.component('navbar-tasks', Navbar);
-
-App.use(({ $store: { commit }, $router }) => {
-    console.log('routes\t', routes);
-    $router.addRoutes(routes);
-    commit('packages/registerNavbar', { component: 'navbar-tasks' });
-});
+commit('packages/registerNavbar', { component: 'navbar-tasks' });
