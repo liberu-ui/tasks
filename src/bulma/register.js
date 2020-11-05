0959ab230e2fdcd66a796f30f7a08095cba559d2
reza <raftx24@gmail.com>
2020-11-05 18:47:24 +0330

    adds auto discovery

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import App from '@enso-ui/ui/src/core/app';
-import routeImporter from '@enso-ui/ui/src/modules/importers/routeImporter';
 import Tasks from './components/navbar/Tasks.vue';
-import './icons';
-
-const routes = routeImporter(require.context('./routes', false, /.*\.js$/));
-App.addRoutes(routes);
 
 Vue.component('navbar-tasks', Tasks);
 App.registerNavbarItem('navbar-tasks', 100);
